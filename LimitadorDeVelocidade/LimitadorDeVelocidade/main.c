0e9c34386ee3f0c3ba1ee6efd91f5e201f49e468
Matheus Holanda Matos <matheus-holanda@hotmail.com>
2019-10-09 17:31:02 -0300

    Update LimitadorDeVelocidade - Speed and Converter

--- LimitadorDeVelocidade/LimitadorDeVelocidade/main.c
+++ LimitadorDeVelocidade/LimitadorDeVelocidade/main.c
@@ -5,6 +5,7 @@
  * Author : Matheus
  */ 
 
+#include <math.h>
 #include <avr/io.h>
 #include <util/delay.h>
 #include <string.h>
@@ -88,9 +89,13 @@ void lcd_clear(){
 }
 /* ------------------------------------------ INTERRUPT ---------------------------------------- */
 
+float count;
+
 ISR(PCINT2_vect){
+
 	if((PIND&(1 << 2))){
 		toogleBit(PORTC,3);
+		count++;
 	}
 }
 
@@ -104,8 +109,84 @@ void interrupt_init(){
 
 /* --------------------------------------------- SPEED ------------------------------------------ */
 
-void speed(){
+// reverses a string 'str' of length 'len' 
+void reverse(char *str, int len) 
+{ 
+    int i=0, j=len-1, temp; 
+    while (i<j) 
+    { 
+        temp = str[i]; 
+        str[i] = str[j]; 
+        str[j] = temp; 
+        i++; j--; 
+    } 
+} 
+
+ // Converts a given integer x to string str[].  d is the number 
+ // of digits required in output. If d is more than the number 
+ // of digits in x, then 0s are added at the beginning. 
+int intToStr(int x, char str[], int d) 
+{ 
+    int i = 0; 
+    while (x) 
+    { 
+        str[i++] = (x%10) + '0'; 
+        x = x/10; 
+    } 
+  
+    // If number of digits required is more, then 
+    // add 0s at the beginning 
+    while (i < d) 
+        str[i++] = '0'; 
+  
+    reverse(str, i); 
+    str[i] = '\0'; 
+    return i; 
+} 
+
+// Converts a floating point number to string. 
+void ftoa(float n, char *res, int afterpoint) 
+{ 
+    // Extract integer part 
+    int ipart = (int)n; 
+  
+    // Extract floating part 
+    float fpart = n - (float)ipart; 
+  
+    // convert integer part to string 
+    int i = intToStr(ipart, res, 0); 
+  
+    // check for display option after point 
+    if (afterpoint != 0) 
+    { 
+        res[i] = '.';  // add dot 
+  
+        // Get the value of fraction part upto given no. 
+        // of points after dot. The third parameter is needed 
+        // to handle cases like 233.007 
+        fpart = fpart * pow(10, afterpoint); 
+  
+        intToStr((int)fpart, res + i + 1, afterpoint); 
+    } 
+} 
+
+char vmax;
+char x[5];
+
+void speed(float tempo, float vmax){
 	
+	float distancia = 0.06;
+	float velocidade = 0.0;
+	float vatual = 0.0;
+	
+	velocidade = (tempo/distancia) * 3.6;
+	
+	vatual += velocidade; 
+	
+	ftoa(vatual, x, 5);
+	for(int i = 0; i < 5; i++){
+		lcd_dado(x[i]);
+	}
 	
 }
 
@@ -124,7 +205,6 @@ void welcome(){
 }
 
 char tx;
-char vmax;
 
 int main(void)
 {
