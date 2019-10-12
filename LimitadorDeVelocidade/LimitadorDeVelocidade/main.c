789b25cd2d4bb60258bc3da2c45131aed23cd40b
Matheus <matheus-holanda@hotmail.com>
2019-10-11 22:29:14 -0300

    Update done fix

--- LimitadorDeVelocidade/LimitadorDeVelocidade/main.c
+++ LimitadorDeVelocidade/LimitadorDeVelocidade/main.c
@@ -113,7 +113,6 @@ void interrupt_init(){
 
 /* --------------------------------------------- SPEED ------------------------------------------ */
 
-// reverses a string 'str' of length 'len' 
 void reverse(char *str, int len) 
 { 
     int i=0, j=len-1, temp; 
@@ -126,9 +125,6 @@ void reverse(char *str, int len)
     } 
 } 
 
- // Converts a given integer x to string str[].  d is the number 
- // of digits required in output. If d is more than the number 
- // of digits in x, then 0s are added at the beginning. 
 int intToStr(int x, char str[], int d) 
 { 
     int i = 0; 
@@ -138,8 +134,6 @@ int intToStr(int x, char str[], int d)
         x = x/10; 
     } 
   
-    // If number of digits required is more, then 
-    // add 0s at the beginning 
     while (i < d) 
         str[i++] = '0'; 
   
@@ -148,26 +142,19 @@ int intToStr(int x, char str[], int d)
     return i; 
 } 
 
-// Converts a floating point number to string. 
 void ftoa(float n, char *res, int afterpoint) 
 { 
-    // Extract integer part 
+
     int ipart = (int)n; 
-  
-    // Extract floating part 
+
     float fpart = n - (float)ipart; 
-  
-    // convert integer part to string 
+
     int i = intToStr(ipart, res, 0); 
-  
-    // check for display option after point 
+
     if (afterpoint != 0) 
     { 
-        res[i] = '.';  // add dot 
-  
-        // Get the value of fraction part upto given no. 
-        // of points after dot. The third parameter is needed 
-        // to handle cases like 233.007 
+        res[i] = '.';
+
         fpart = fpart * pow(10, afterpoint); 
   
         intToStr((int)fpart, res + i + 1, afterpoint); 
