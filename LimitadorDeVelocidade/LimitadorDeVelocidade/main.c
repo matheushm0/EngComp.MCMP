5c22262bd6aa306cb1fe8b4ed3fb8716915c5e36
Matheus Holanda Matos <matheus-holanda@hotmail.com>
2019-10-20 16:01:37 -0300

    End update fix LimitadorDeVelocidade

--- LimitadorDeVelocidade/LimitadorDeVelocidade/main.c
+++ LimitadorDeVelocidade/LimitadorDeVelocidade/main.c
@@ -16,9 +16,17 @@
 #define BAUD 4800UL
 #define MYUBRR ((unsigned long)((F_CPU/(16*BAUD))-1))
 
-#define toogleBit(valor,bit) (valor ^= (1<<bit))
 #define setBit(valor,bit) (valor |= (1<<bit))
 #define clearBit(valor,bit) (valor &= ~(1<<bit))
+#define toogleBit(valor,bit) (valor ^= (1<<bit))
+#define testBit(valor,bit)    (valor & (1<<bit))
+
+
+#define LCD_Dir  DDRB			
+#define LCD_Port PORTB			
+#define RS PB1				
+#define EN PB2 				
+
 
 /* -------------------------------------------- SERIAL ----------------------------------------- */
 
@@ -54,28 +62,47 @@ void USART_putstring(char* StringPtr){
 /* --------------------------------------------- LCD ------------------------------------------- */
 
 void lcd_init(){
-	lcd_comando(0x38);
-	lcd_comando(0x0E);
-	lcd_comando(0x06);
-	lcd_comando(0x01);
+	LCD_Dir = 0xFF;			
+	_delay_ms(20);			
+	
+	lcd_comando(0x02);		
+	lcd_comando(0x28);             
+	lcd_comando(0x0c);             
+	lcd_comando(0x06);              
+	lcd_comando(0x01);              
+	_delay_ms(2);
 }
 
 void lcd_comando(char comando){
-	PORTB = comando;
-	PORTC &= ~(0x01);
-	PORTC &= ~(0x02);
-	PORTC |= (0x04);
-	_delay_ms(50);
-	PORTC &= ~(0x04);
+	LCD_Port = (LCD_Port & 0x0F) | (comando & 0xF0);
+	LCD_Port &= ~ (1<<RS);		
+	LCD_Port |= (1<<EN);		
+	_delay_us(1);
+	LCD_Port &= ~ (1<<EN);
+
+	_delay_us(200);
+
+	LCD_Port = (LCD_Port & 0x0F) | (comando << 4); 
+	LCD_Port |= (1<<EN);
+	_delay_us(1);
+	LCD_Port &= ~ (1<<EN);
+	_delay_ms(2);
 }
 
 void lcd_dado(char dado){
-	PORTB = dado;
-	PORTC |= (0x01);
-	PORTC &= ~(0x02);
-	PORTC |= (0x04);
-	_delay_ms(50);
-	PORTC &= ~(0x04);
+	LCD_Port = (LCD_Port & 0x0F) | (dado & 0xF0); 
+	LCD_Port |= (1<<RS);		
+	LCD_Port|= (1<<EN);
+	_delay_us(1);
+	LCD_Port &= ~ (1<<EN);
+
+	_delay_us(200);
+
+	LCD_Port = (LCD_Port & 0x0F) | (dado << 4);
+	LCD_Port |= (1<<EN);
+	_delay_us(1);
+	LCD_Port &= ~ (1<<EN);
+	_delay_ms(2);
 	
 }
 
@@ -91,28 +118,38 @@ void lcd_clear(){
 }
 /* ------------------------------------------ INTERRUPT ---------------------------------------- */
 
-float count;
-float tempo; 
+volatile uint8_t count;
+float tempo = 0; 
+volatile unsigned int tempo1=0;
+volatile unsigned int tempo2=0;
+float periodo = 0;
 
-ISR(PCINT2_vect){
-
-	if((PIND&(1 << 2))){
-		toogleBit(PORTC,3);
-		count++;
+ISR(TIMER1_CAPT_vect){            
+	
+	if(count==0){
+		tempo1 = ICR1;
+		TIFR1=(1<<ICF1);
 	}
-	tempo = count / 60;
+	if(count==1){
+		tempo2 = ICR1;
+		TIFR1=(1<<ICF1);
+	}
+	count++;
 }
 
+
 void interrupt_init(){
-	
-	PCICR = 0x04;
-	PCMSK2 = 0x0c;
+	setBit(DDRB,7);
+	clearBit(DDRB,0);
+	setBit(TCCR1B,0);
+	setBit(TCCR1B,6);
+	setBit(TIMSK1,5);
+	USART_Init(MYUBRR);
 	sei();
-	
 }
 
 /* --------------------------------------------- SPEED ------------------------------------------ */
-
+ 
 void reverse(char *str, int len) 
 { 
     int i=0, j=len-1, temp; 
@@ -133,7 +170,7 @@ int intToStr(int x, char str[], int d)
         str[i++] = (x%10) + '0'; 
         x = x/10; 
     } 
-  
+
     while (i < d) 
         str[i++] = '0'; 
   
@@ -153,7 +190,7 @@ void ftoa(float n, char *res, int afterpoint)
 
     if (afterpoint != 0) 
     { 
-        res[i] = '.';
+        res[i] = '.'; 
 
         fpart = fpart * pow(10, afterpoint); 
   
@@ -166,13 +203,13 @@ float vmax;
 
 void speed(float tempo, float vmax){
 	
-	float distancia = 0.06;
+	float distancia = 0.1828;
 	float velocidade = 0;
 	float vatual = 0;
 	
-	velocidade = (tempo/distancia) * 3.6;
+	velocidade = (distancia/tempo) * 3.6;
 	
-	vatual += velocidade; 
+	vatual = velocidade;
 	
 	ftoa(vatual, x, 3);
 
@@ -180,9 +217,13 @@ void speed(float tempo, float vmax){
 		lcd_dado(x[i]);
 	}
 	
-	if(vatual > vmax){
+	if(velocidade > vmax){
 		setBit(PORTC,4);
 	}
+	if(velocidade < vmax){
+		clearBit(PORTC,4);
+	}
+	
 	
 }
 
@@ -201,10 +242,10 @@ void welcome(){
 }
 
 char tx[4];
+char t[5];
 
 int main(void)
 {
-	DDRB = 0xff;
 	DDRC = 0xff;
 	
 	interrupt_init();
@@ -229,12 +270,20 @@ int main(void)
 				
 				atualiza:
 				
-				lcd_comando(0x88);
-				speed(tempo, vmax);
-				_delay_ms(100);
+				if(count == 2){
+					count = 0;
+					
+					periodo = (tempo2-tempo1);
+					tempo = periodo/1000000;
+
+					USART_Transmit(0x0d);
+					lcd_comando(0x88);
+					speed(tempo, vmax);
+					_delay_ms(100);
+					
+				}		
 				goto atualiza;
 			}
 		}
     }
 }
-
